b440b3646f0a9e4d6778a525da15cb10e150d3f8
Klaus Ening <klaus.ening@sintef.no>
2020-12-24 15:39:52 +0100

    logging, wait for connection for pull

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 cd /home/en/tv-video-call
+echo "--------- $(date) ---------" >> vidlog.txt
+while ! ping -c 1 -W 1 8.8.8.8; do
+    echo "Waiting for 8.8.8.8 - network interface might be down..." >> vidlog.txt
+    sleep 1
+done
 git checkout update
 git pull
-python3 main.py --room $VIDROOM --user $VIDNAME --server $VIDSERVER > vidlog.txt
+python3 main.py --room $VIDROOM --user $VIDNAME --server $VIDSERVER >> vidlog.txt
