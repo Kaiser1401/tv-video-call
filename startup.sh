8fd816368a87a426e8186446eb725b3014b25004
Kaiser1401 <k.ening@gmx.de>
2020-12-25 14:41:12 +0100

    autostart and env difficulties on lubuntu ....

--- startup.sh
+++ startup.sh
@@ -1,5 +1,3 @@
-source $HOME/.bashrc
-cd $HOME/tv-video-call
 echo "--------- $(date) ---------" >> vidlog.txt
 while ! ping -c 1 -W 1 8.8.8.8; do
     echo "Waiting for 8.8.8.8 - network interface might be down..." >> vidlog.txt
@@ -7,4 +5,7 @@ while ! ping -c 1 -W 1 8.8.8.8; do
 done
 git checkout update
 git pull
+echo $VIDROOM
+echo $VIDNAME
+echo $VIDSERVER
 python3 main.py --room $VIDROOM --user $VIDNAME --server $VIDSERVER >> vidlog.txt
